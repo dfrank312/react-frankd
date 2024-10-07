f75b632a45debffd6dc271e70f85490878a07e97
dfrank312 <mikazeai6153@gmail.com>
2024-10-07 14:27:07 +0600

    -

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Header from './elements/Header/Header';
 import Footer from './elements/Footer/Footer';
 import { useState } from 'react';
-import { useState } from 'react';
 
 import Maintab from './elements/Tabs/Main/maintab';
 import ProductsTab from './elements/Tabs/Products/products';
